2eb1cfcb27991893edb7c649363b82351a56cb75
damian ojeda <damianpolzlsoftware@gmail.com>
2019-11-19 19:20:50 -0300

    ES6

---
+++ babelrc.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Babelrc extends Component {
+	
+	render(){
+		return(
+			<View>
+				<Text> Services ES6 </Text>
+			</View>
+		)
+	}
+
+}
+
+export default Babelrc
